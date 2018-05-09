8d4515d483531dce625623aaaffeff81b3ad4932
k-cafe <hekouki111@gmail.com>
2018-05-09 21:52:47 +0900

    fix indicator

--- TabIndicator/app/src/main/java/com/example/kohi/tabindicator/main/MainActivity.kt
+++ TabIndicator/app/src/main/java/com/example/kohi/tabindicator/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import com.example.kohi.tabindicator.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,15 +21,27 @@ class MainActivity : AppCompatActivity() {
 
         colorViewModel = ColorViewModel()
 
-        pagerView.adapter = ColorPagerAdapter(supportFragmentManager)
-        pagerView.offscreenPageLimit = 2 
-        pagerView.addOnPageChangeListener(pageChangeListener)
-        indicatorLayout.setupWithViewPager(pagerView, true)
+        initializePageViewer()
+        initializeIndicator()
     }
 
     //
     // setUps
     //
+
+    private fun initializePageViewer() {
+        pagerView.adapter = ColorPagerAdapter(supportFragmentManager)
+        pagerView.offscreenPageLimit = 2
+        pagerView.addOnPageChangeListener(pageChangeListener)
+    }
+
+    private fun initializeIndicator() {
+        indicatorLayout.setupWithViewPager(pagerView, true)
+        for (tabNo in 0..pageCount) {
+            indicatorLayout.getTabAt(tabNo)?.icon = ContextCompat.getDrawable(this, R.drawable.selector_indicator)
+        }
+    }
+
     private val pageChangeListener = object : ViewPager.SimpleOnPageChangeListener() {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
@@ -39,6 +52,7 @@ class MainActivity : AppCompatActivity() {
     //
     // inner class
     //
+
     private inner class ColorPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
         override fun getItem(position: Int) = ColorFragment.putExtra(position).setModel(colorViewModel)
